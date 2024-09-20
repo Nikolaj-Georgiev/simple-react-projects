d2bf0a553daa7783ce064acb33ed762f952fe7f9
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-09-20 12:46:38 +0300

    fixed header for match list

--- football-tournament/src/components/home/MatchList.jsx
+++ football-tournament/src/components/home/MatchList.jsx
@@ -3,23 +3,23 @@ import Match from '../match/Match';
 export default function MatchList({ stage, selectedDate, onBack }) {
   return (
     <div className='match-list'>
-      <button
-        className='button'
-        onClick={onBack}
-      >
-        Back to Dates
-      </button>
-      <div>
+      <div className='match-list__header'>
         <h3 className='match-list__heading'>{`${stage.name} - ${selectedDate}`}</h3>
-        <ul className='match-list__matches'>
-          {stage.data[selectedDate].map((match) => (
-            <Match
-              key={match.id}
-              match={match}
-            />
-          ))}
-        </ul>
+        <button
+          className='button'
+          onClick={onBack}
+        >
+          Back to Dates
+        </button>
       </div>
+      <ul className='match-list__matches'>
+        {stage.data[selectedDate].map((match) => (
+          <Match
+            key={match.id}
+            match={match}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
