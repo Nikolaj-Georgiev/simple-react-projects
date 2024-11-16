7d6726512b7f484e8e56daba760aefe13d2c188e
Nikolay Georgiev <47087200+Nikolaj-Georgiev@users.noreply.github.com>
2024-11-16 19:52:06 +0200

    changed logic to remove submit button

--- reusable-gauge/src/App.jsx
+++ reusable-gauge/src/App.jsx
@@ -1,5 +1,3 @@
-// import ComponentName from './components/ComponentName';
-// import Gauge from './components/Gauge';
 import { useState } from 'react';
 import GaugeRevised from './components/GaugeRevised';
 
@@ -7,13 +5,13 @@ function App() {
   const [sizeValue, setSizeValue] = useState('');
   const [gaugeIndex, setGaugeIndex] = useState(0);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const size = formData.get('size');
-    const index = formData.get('index');
-    setSizeValue(size);
-    setGaugeIndex(Number(index));
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'size') {
+      setSizeValue(value);
+    } else if (name === 'index') {
+      setGaugeIndex(Number(value));
+    }
   };
 
   return (
@@ -23,30 +21,29 @@ function App() {
           size={sizeValue}
           value={gaugeIndex}
         />
-        <form onSubmit={handleSubmit}>
-          <div className='input-container'>
-            <div className='size'>
-              <label htmlFor='size'>Size</label>
-              <select
-                id='size'
-                name='size'
-              >
-                <option value='small'>small</option>
-                <option value='medium'>medium</option>
-                <option value='large'>large</option>
-              </select>
-            </div>
-            <div className='index'>
-              <label htmlFor='index'>Index value</label>
-              <input
-                type='number'
-                id='index'
-                name='index'
-              />
-            </div>
+        <div className='input-container'>
+          <div className='size'>
+            <label htmlFor='size'>Size</label>
+            <select
+              id='size'
+              name='size'
+              onChange={handleChange}
+            >
+              <option value='small'>small</option>
+              <option value='medium'>medium</option>
+              <option value='large'>large</option>
+            </select>
           </div>
-          <button>submit</button>
-        </form>
+          <div className='index'>
+            <label htmlFor='index'>Index value</label>
+            <input
+              type='number'
+              id='index'
+              name='index'
+              onChange={handleChange}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
